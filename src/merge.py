cc14036b35a39f30434558737787fd3cf9b7c047
KJSell <112373306+KJSell@users.noreply.github.com>
2022-09-16 08:12:15 +0200

    mergeV7

--- src/merge.py
+++ src/merge.py
@@ -1,30 +1,20 @@
 """Code for merging two sorted lists."""
 
+
 def merge(x: list[int], y: list[int]) -> list[int]:
-    """
-    Merge two sorted lists.
-
-    Returns a list that contains all the elements in x and y
-    in sorted order.
-
-    >>> merge([1, 2, 4, 6], [1, 3, 4, 5])
-    [1, 1, 2, 3, 4, 4, 5, 6]
-    """
     i, j = 0, 0
-    z = []  # a new list to copy elements into
-    # FIXME: fill out the loop so you merge the lists
-    # until one of them is empty
+    z = []
+    big_number = float('inf') #7
+    x.append(big_number)
+    y.append(big_number)
     while i < len(x) and j < len(y):
         if x[i] <= y[j]:
-            z.append(x[i]) 
+            z.append(x[i])
             i += 1
-        if y[j] < x[i]: 
+        else:
             z.append(y[j])
             j += 1
-        if len(z) == (len(x) + len(y)):
-            pass
-    # FIXME: you shouldn't just break here
-    # At least one of the lists is empty now. Copy the
-    # remainder of the other into z.
+        print((i,j))
+    z.remove(big_number)
     return z
-merge([1, 2, 4, 6], [1, 3, 4, 5])
+print(merge([1, 2, 4, 6], [1, 3, 4, 5]))
